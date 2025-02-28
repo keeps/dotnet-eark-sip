f1537e783fee0ca7b5730df8d257ab840b23a100
boticas <zezinhoboticas@gmail.com>
2025-02-28 12:05:12 +0000

    fix representation utils: include folder in representation

--- dotnet-eark-sip/src/Utils/RepresentationUtils.cs
+++ dotnet-eark-sip/src/Utils/RepresentationUtils.cs
@@ -13,17 +13,16 @@ public class RepresentationUtils {
 
   public static void IncludeInRepresentation(string directory, IPRepresentation representation, List<string> relativeFolders) {
     if (Directory.Exists(directory)) {
-      List<string> newRelativeFolders = new List<string>(relativeFolders);
-      newRelativeFolders.AddRange(directory.Split(Path.DirectorySeparatorChar));
-
       IEnumerable<string> subDirectories = Directory.GetDirectories(directory);
       foreach (string subDirectory in subDirectories) {
+        string directoryName = subDirectory.Split(Path.DirectorySeparatorChar).Last();
+        List<string> newRelativeFolders = new List<string>(relativeFolders) { directoryName };
         IncludeInRepresentation(subDirectory, representation, newRelativeFolders);
       }
 
       IEnumerable<string> files = Directory.GetFiles(directory);
       foreach (string file in files) {
-        AddToRepresentation(file, representation, newRelativeFolders);
+        AddToRepresentation(file, representation, relativeFolders);
       }
     }
   }
