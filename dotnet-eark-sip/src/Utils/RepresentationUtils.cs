e19a0081bfefe34c549ed4711f031e1a9004f7ba
boticas <zezinhoboticas@gmail.com>
2025-03-03 16:46:13 +0000

    extend IncludeInRepresentation to include files

--- dotnet-eark-sip/src/Utils/RepresentationUtils.cs
+++ dotnet-eark-sip/src/Utils/RepresentationUtils.cs
@@ -11,19 +11,21 @@ public class RepresentationUtils {
     IncludeInRepresentation(directory, representation, new());
   }
 
-  public static void IncludeInRepresentation(string directory, IPRepresentation representation, List<string> relativeFolders) {
-    if (Directory.Exists(directory)) {
-      IEnumerable<string> subDirectories = Directory.GetDirectories(directory);
+  public static void IncludeInRepresentation(string path, IPRepresentation representation, List<string> relativeFolders) {
+    if (Directory.Exists(path)) {
+      IEnumerable<string> subDirectories = Directory.GetDirectories(path);
       foreach (string subDirectory in subDirectories) {
         string directoryName = subDirectory.Split(Path.DirectorySeparatorChar).Last();
         List<string> newRelativeFolders = new List<string>(relativeFolders) { directoryName };
         IncludeInRepresentation(subDirectory, representation, newRelativeFolders);
       }
 
-      IEnumerable<string> files = Directory.GetFiles(directory);
+      IEnumerable<string> files = Directory.GetFiles(path);
       foreach (string file in files) {
         AddToRepresentation(file, representation, relativeFolders);
       }
+    } else if (File.Exists(path)) {
+      AddToRepresentation(path, representation, relativeFolders);
     }
   }
 
